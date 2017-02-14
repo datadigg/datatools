049440d60e7fdc23b33b63a80ffbd4a5abaece34
xucheng <bd66_6@hotmail.com>
2017-02-14 15:50:14 +0800

    support multiple transformer mappings and loader collections

--- etl/etl_mongo.py
+++ etl/etl_mongo.py
@@ -6,6 +6,7 @@ from etl import DataExtractor,DataLoader,DataTransformer
 class MongoDataExtractor(DataExtractor):
     def __init__(self, config):
         mongodb = config.settings.extractor.mongodb
+        logging.debug('*' * 30)
         logging.debug('MongoDataExtractor init:')
         logging.debug('host:%s, port:%s' % (mongodb.host, mongodb.port))
         logging.debug('db:%s' % mongodb.db)
@@ -38,10 +39,9 @@ class MongoDataTransformer(DataTransformer):
     def _clean_str(self, val):
         return ''.join((e.isalnum() and e or '') for e in val).upper()
     
-    def transform(self, row):
+    def _transform(self, fields, row):
         query = {}
         update = {}
-        fields = self.config.settings.transformer.mapping.fields
         for field in fields:
             val = row.get(field.source)
             indexed = getattr(field, "indexed", False)
@@ -66,26 +66,55 @@ class MongoDataTransformer(DataTransformer):
                            
         if query:
             return query,update
+    
+    def transform(self, row):
+        mapping = self.config.settings.transformer.mapping
+        if isinstance(mapping, list):
+            data = {}
+            for conf in mapping:
+                result = self._transform(conf.fields, row)
+                if result:
+                    data[conf.tag] = result
+        else:
+            data = self._transform(mapping.fields, row)
+        return data
 
 class MongoDataLoader(DataLoader):
     def __init__(self, config):
         mongodb = config.settings.loader.mongodb
+        logging.debug('*' * 30)
         logging.debug('MongoDataLoader init:')
         logging.debug('host:%s, port:%s' % (mongodb.host, mongodb.port))
-        logging.debug('db:%s' % mongodb.db)
-        logging.debug('collection:%s' % mongodb.collection)
 
-        self.mongodb = mongodb
         self.client = MongoClient(mongodb.host, mongodb.port)
-        self.db = self.client[mongodb.db]
-        if mongodb.username:
-            self.db.authenticate(mongodb.username, mongodb.password)
-        self.collection = self.db[mongodb.collection]
-        self.collection.ensure_index(mongodb.indexKey, unique=True)
+        if hasattr(mongodb, 'dbs'):
+            self.collections = {}
+            for conf in mongodb.dbs:
+                logging.debug('-' * 30)
+                logging.debug('tag:%s' % conf.tag)
+                collection = self._get_collection(conf)
+                self.collections[conf.tag] = collection
+        else:
+            self.collection = self._get_collection(mongodb)
 
+    def _get_collection(self, conf):
+        logging.debug('db:%s' % conf.db)
+        logging.debug('collection:%s' % conf.collection)
+        db = self.client[conf.db]
+        if hasattr(conf, 'username') and conf.username:
+            db.authenticate(conf.username, conf.password)
+        collection = db[conf.collection]
+        collection.ensure_index(conf.indexKey, unique=True)
+        return collection
+                
     def load(self, datacol):
-        for query,update in datacol:
-            self.collection.update(query, update, upsert=True)
+        for data in datacol:
+            if isinstance(data, dict):
+                for key, (query, update) in data.iteritems():
+                    self.collections[key].update(query, update, upsert=True)
+            else:
+                query, update = data
+                self.collection.update(query, update, upsert=True)
 
     def close(self):
         self.client.close()
