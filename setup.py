6c20f099cdfcc8d91827c98637efb62785b54e74
tutunannan <bd66_6@hotmail.com>
2021-10-13 17:59:18 +0800

    fix pip install parse_requirements error

--- setup.py
+++ setup.py
@@ -1,20 +1,12 @@
-from setuptools import setup, find_packages
-try:  # for pip >= 10
-    from pip._internal.req import parse_requirements
-except ImportError:  # for pip <= 9.0.3
-    from pip.req import parse_requirements
-
 import os
-import uuid
+from setuptools import find_packages
+from setuptools import setup
 
-# parse_requirements() returns generator of pip.req.InstallRequirement objects
-BASE_DIR = os.path.dirname(os.path.realpath(__file__))
-reqs_file = os.path.join(BASE_DIR, 'requirements.txt')
-install_reqs = parse_requirements(reqs_file, session=uuid.uuid1())
 
-# reqs is a list of requirement
-# e.g. ['django==1.5.1', 'mezzanine==1.4.6']
-reqs = [str(ir.req) for ir in install_reqs]
+def parse_requirements(filename):
+    with open(filename) as requirements_file:
+        return requirements_file.readlines()
+
 
 setup(
     # Application name:
@@ -43,5 +35,5 @@ setup(
     long_description=open("README.md").read(),
 
     # Dependent packages (distributions)
-    install_requires=reqs,
+    install_requires=parse_requirements('requirements.txt'),
 )
